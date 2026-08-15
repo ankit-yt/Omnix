6d00e0f86aad11dd517eeb8072e48766e74d8fb0
ankit-yt <ankitpk303@gmail.com>
2026-08-15 13:13:36 +0530

    logout fixed

--- frontend/src/hooks/useLogout.ts
+++ frontend/src/hooks/useLogout.ts
@@ -1,13 +1,22 @@
 import { authService } from "@/services/auth.service";
 import { useRouter } from "next/navigation";
+import { useAuthStore } from "@/store/useAuthStore";
 
 export function useLogout() {
   const router = useRouter();
+  const clearStore = useAuthStore((state) => state.logout);
+
   const logout = async () => {
-    await authService.logout();
-    router.replace("/")
+    try {
+      await authService.logout();
+    } catch (error) {
+      console.error("Backend logout failed:", error);
+    } finally {
+      clearStore();
+
+      router.replace("/");
+    }
   };
 
   return logout;
-
 }
